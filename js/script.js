a6dc9cc550c024f4034efd107abbbe03ed8d690f
Azri <53987614+kihonataa@users.noreply.github.com>
2025-09-30 02:48:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ document.querySelector('#hamburger-menu').onclick = () => {
 // klik diluar sidebar
 const hamburger = document.querySelector ('#hamburger-menu');
 
-document.addEventListener('click', function(e) { 
+document.addEventListener('click', function (e) { 
     if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+
 });
